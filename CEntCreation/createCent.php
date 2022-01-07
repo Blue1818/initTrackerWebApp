b33542740adff1f5bb0629717488ac95f383f944
Blue1818 <iwadale@gmail.com>
2022-01-07 15:39:52 -0600

    first commit

---
+++ CEntCreation/createCent.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title>Page Title</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+</head>
+<body>
+
+<?php
+
+//print post values
+echo $_POST["CentName"];
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
