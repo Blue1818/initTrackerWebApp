02fd8c7d87d63f9f0719adbdcaf7dda548157b3b
Blue1818 <iwadale@gmail.com>
2022-06-14 12:31:44 -0500

    add sudo code

---
+++ entity.cpp
@@ -0,0 +1,16 @@
+#include "entity.h"
+
+
+
+
+
+entity::entity()
+{
+    //set vars to default values
+    name = "default";
+    rollVal = 0;
+    initMod = 0;
+    dexScore = 0;
+    lucky = false;
+}
+
---
+++ entity.h
@@ -0,0 +1,41 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <string>
+
+using std::string;
+
+
+class entity
+{
+    private:
+    // vars:
+    // name -> string
+    string name;
+    // rollVal -> int from 1 - 20 representing the die value
+    int rollVal;
+    // initMod -> signed moddifier to init excluding dex
+    int initMod;
+    // dexScore -> unsigned int (used to determin dexmod)
+    int dexScore;
+    // lucky -> binary val for rerolling 1's
+    bool lucky;
+
+
+    public:
+    // methods:
+    // default constructor() -> set everything to 0
+    entity();
+    // constructor(name, rollVal, initMod, dexScore, lucky)
+    // signed int dexmod -> ((desScore / 2) - 5)
+    // signed int fullMod -> (dexmod + initMod) 
+    // getters and setters
+    // roll -> get random int 1 to 20, assign to rollVal. (reroll on 1 if lucky = true)
+    // signed int getInit -> (rollVal + fullMod())
+    
+
+
+};
+
+
+#endif
