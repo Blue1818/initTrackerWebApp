72435fbd9be6625b7bbdebb0c0d3ceff453c791b
Dale Westberg <dwestwork18@gmail.com>
2022-06-16 13:10:51 -0500

    Create rpnDice.h

---
+++ rpnDiceCalc/rpnDice.h
@@ -0,0 +1,29 @@
+#ifndef RPNDICE_H
+#define RPNDICE_H
+
+#include <string>
+#include <stack>
+
+
+class rpnDice
+{
+    private:
+    std::stack work;
+    string expression;
+
+
+
+
+    public:
+    rpnDice()
+    {
+        this->expression = "X"
+    }
+
+
+
+
+
+};
+
+#endif
