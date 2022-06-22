b7df7ac6af1eda688697d5875469a8acfcbbc45c
Dale Westberg <dwestwork18@gmail.com>
2022-06-22 16:41:14 -0500

    unitTest added

---
+++ entity/unitTest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# make script for unit tests
+
+
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o entity.o entity.cpp
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o entTest.o entTest.cpp
+
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -o entUnitTest entTest.o entity.o
+
+#  remove o file
+rm -f *.o entity.o
+rm -f *.o entTest.o
--- rpnDiceCalc/unitTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# make script for unit tests
-
-
-#g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o reduce.o reduce.cpp
-g++ -g -ansi -pedantic -Wall -Werror -std=c++14 reduce.cpp -pthread -o reduce
-
-# remove o files
-#rm -f *.o reduce.o
