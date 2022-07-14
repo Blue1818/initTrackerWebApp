5284360db3b3758dc0a22f38efe4810335591b0e
Dale Westberg <dwestwork18@gmail.com>
2022-07-14 13:59:48 -0500

    add shuffle function

--- initmanager/initmanager.cpp
+++ initmanager/initmanager.cpp
@@ -1,3 +1,21 @@
 #include "initmanager.h"
 
+//use to shuffle tieOrder before sorted.
+//this order will be used to brake ties
+void initmanager::shuffle()
+{
+    int numS = tieOrder.size();
+    int randNum = 0;
+    entity temp;
 
+    //for each element in tie order
+    for (int i = 0; i < numS; i++)
+    {
+        //get a random possition between i and (numS - 1)
+        randNum = (rand() % (numS - i)) + i;
+        //swap it with i
+        temp = tieOrder.at(i);
+        tieOrder.at(i) = tieOrder.at(randNum);
+        tieOrder.at(randNum) = temp;
+    }
+}
