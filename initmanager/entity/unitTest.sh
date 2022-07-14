5de0c1edb246c0884420780d59a2f69b5271a357
Dale Westberg <dwestwork18@gmail.com>
2022-07-14 13:32:31 -0500

    Update unitTest.sh

--- initmanager/entity/unitTest.sh
+++ initmanager/entity/unitTest.sh
@@ -2,7 +2,7 @@
 # make script for unit tests
 
 
-g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o entity.o entity.cpp
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o entity.o entity/entity.cpp
 g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o entTest.o entTest.cpp
 
 g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -o entUnitTest entTest.o entity.o
