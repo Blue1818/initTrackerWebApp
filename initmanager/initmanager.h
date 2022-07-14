a966d7014284c8a58aba1b8ef17f76ddba2178c6
Dale Westberg <dwestwork18@gmail.com>
2022-07-14 13:32:46 -0500

    class skelly

--- initmanager/initmanager.h
+++ initmanager/initmanager.h
@@ -1,10 +1,39 @@
 #ifndef INITMANAGER_H
 #define INITMANAGER_H
 
+#include <vector>
+
+#include "entity/entity.h"
+
+using std::vector;
 
 class initmanager
 {
+    public:
+    //vector to hold all entities in tie order.
+    vector<entity> tieOrder;
 
+
+
+    private:
+    //constructor
+    initmanager();
+
+    //shuffle tieOrder
+    void shuffle();
+
+    //sort tieOrder
+    void sorttieOrder();
+
+    //start encounter
+    void startEncounter();
+
+    //run round
+    void runRound();
+
+    //player turn
+    void runTurn(entity* character);
+    
 
 
 
