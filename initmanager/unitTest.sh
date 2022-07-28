5285f7e343d25daac569aba102010b69a69e683c
Dale Westberg <dwestwork18@gmail.com>
2022-07-27 19:45:18 -0500

    Update unitTest.sh

--- initmanager/unitTest.sh
+++ initmanager/unitTest.sh
@@ -2,13 +2,15 @@
 # make script for unit tests
 
 
-g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o entity.o entity/entity.cpp
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o entity.o creep/entity/entity.cpp
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o creep.o creep/creep.cpp
 g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o initmanager.o initmanager.cpp
 g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o unitTest.o unitTest.cpp
 
-g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -o unitTest unitTest.o initmanager.o entity.o
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -o unitTest unitTest.o initmanager.o creep.o entity.o
 
 #  remove o file
 rm -f *.o entity.o
+rm -f *.o creep.o
 rm -f *.o initmanager.o
 rm -f *.o unitTest.o
