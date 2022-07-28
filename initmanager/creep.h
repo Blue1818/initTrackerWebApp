f94946c2fec389d5b82b6220839c2e1543dde677
Dale Westberg <dwestwork18@gmail.com>
2022-07-27 19:10:33 -0500

    Update creep.h

--- initmanager/creep.h
+++ initmanager/creep.h
@@ -10,10 +10,18 @@ class creep : public entity
     int roll; // 1 - 20
 
     public:
+    //constructor
+    creep();
     //setters
-
+    void setactMod(int actMod) { this->actMod = actMod; }
+    void setroll(int roll) { this->roll = roll; }
     //getters
+    int getactMod() { return actMod; }
+    int getroll() { return roll; }
 
+    void roll();
+
+    void runTurn();
     
     int result() { return (fullMod() + actMod + roll); }
 
